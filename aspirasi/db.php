4b3ace2b2d026f4dba3b925228ad823efb39cab1
Giovann <66812243+radifti@users.noreply.github.com>
2026-03-30 21:51:19 +0700

    Delete aspirasi directory

--- aspirasi/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'db_aspirasi';
-
-    $conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('gagal terhubung ke database');
-?>
